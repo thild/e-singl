f54ff42f7cc0d2367557cae6227e66daef5efcc9
Tony Alexander Hild <tony_hild@yahoo.com>
2015-11-06 00:02:04 -0200

    changed some css and js includes

--- src/e-Singl/wwwroot/js/modernizr.js
+++ src/e-Singl/wwwroot/js/modernizr.js
@@ -1,1 +1,13 @@
-define(["ModernizrProto"],function(n){var o=function(){};return o.prototype=n,o=new o});
+define(['ModernizrProto'], function(ModernizrProto) {
+  // Fake some of Object.create so we can force non test results to be non "own" properties.
+  var Modernizr = function() {};
+  Modernizr.prototype = ModernizrProto;
+
+  // Leak modernizr globally when you `require` it rather than force it here.
+  // Overwrite name so constructor name is nicer :D
+  Modernizr = new Modernizr();
+
+  return Modernizr;
+});
+
+//# sourceMappingURL=modernizr.js.map
