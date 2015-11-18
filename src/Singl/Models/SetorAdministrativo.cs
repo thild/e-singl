50a4155892556ecbb3cc0c48dd2fb77af664ae71
Tony Alexander Hild <tony_hild@yahoo.com>
2015-11-18 16:59:03 -0200

    changed project name

---
+++ src/Singl/Models/SetorAdministrativo.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Singl.Models
+{
+    public class SetorAdministrativo : ISetorAdministrativo
+    {
+        public SetorAdministrativo()
+        {
+        }
+
+        public string Nome
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
