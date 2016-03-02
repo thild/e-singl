b0b7a6201f8a7313e6d7b51f03073dc5d9dce81b
Tony Alexander Hild <tony_hild@yahoo.com>
2016-03-02 00:25:07 -0300

    Reverted Startup

--- src/Singl/Startup.cs
+++ src/Singl/Startup.cs
@@ -164,17 +164,17 @@ namespace Singl
                 // Uncomment the following line to add a route for porting Web API 2 controllers.
                 // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
             }); 
-// 
-//             if (System.IO.File.Exists("singl.sqlite"))
-//             {
-//                 System.Console.WriteLine("Deleted singl.sqlite");
-//                 System.IO.File.Delete("singl.sqlite");
-//             }
-// 
-//             using (var context = new DatabaseContext())
-//             {
-//                 context.InitializeStoreDatabaseAsync(app.ApplicationServices).Wait();
-//             }
+
+            if (System.IO.File.Exists("singl.sqlite"))
+            {
+                System.Console.WriteLine("Deleted singl.sqlite");
+                System.IO.File.Delete("singl.sqlite");
+            }
+
+            using (var context = new DatabaseContext())
+            {
+                context.InitializeStoreDatabaseAsync(app.ApplicationServices).Wait();
+            }
         }
 
         private void ConfigureMappers()
