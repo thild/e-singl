1d7689d02e54d4e66a47125f2f6d5ebaadd65a6f
Tony Alexander Hild <tony_hild@yahoo.com>
2016-03-02 00:25:07 -0300

    Reverted Startup

--- src/Singl/Controllers/HomeController.cs
+++ src/Singl/Controllers/HomeController.cs
@@ -9,34 +9,5 @@ namespace Singl.Controllers
         {
             return View(new HomeIndexViewModel());
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View("~/Views/Shared/Error.cshtml");
-        }
-        
-        public IActionResult StatusCodePage()
-        {
-            return View("~/Views/Shared/StatusCodePage.cshtml");
-        }
-
-        public IActionResult AccessDenied()
-        {
-            return View("~/Views/Shared/AccessDenied.cshtml");
-        }        
     }
 }
